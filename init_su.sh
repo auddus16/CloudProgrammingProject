53b545a4a9d3a72c31e55bde8916831ce77dd15b
MYONGYEON HWANG <59945024+auddus16@users.noreply.github.com>
2022-06-17 21:25:41 +0900

    Delete init_su.sh

--- init_su.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "------ create default admin user ------"
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@myapp.local', '1234')" | python manage.py shell
